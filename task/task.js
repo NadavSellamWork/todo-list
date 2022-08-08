215425c347c9d065fe67156093442274b2800613
Nadav Sellam <nadav.sellam@gmail.com>
2022-08-08 16:23:10 +0300

    added the code for part 3

--- task/task.js
+++ task/task.js
@@ -1,9 +1,15 @@
 export class Task {
-    constructor(text = "", done = false, onUpdate, onDelete, onDone, onUnDone) {
+    constructor(text = "", done = false, eventHandlers) {
+        let eventHandlersToAssign = {
+            "onUpdate": eventHandlers.onUpdate,
+            "onDelete": eventHandlers.onDelete,
+            "onDone": eventHandlers.onDone,
+            "onUnDone": eventHandlers.onUnDone,
+        };
+        Object.assign(this, eventHandlersToAssign);
         this.done = done;
         this.text = text;
 
-        this.onUpdate = onUpdate;
         this.onDelete = () => { onDelete.bind(this, this) };
         this.onDone = () => { onDone(this) };
         this.onUnDone = () => { onUnDone(this) };
